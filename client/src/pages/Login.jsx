466ac6fcc3b701d62d01d05db1c9b206ff92a723
iam-vinnu <binaybehera101@gmail.com>
2025-01-15 12:14:27 +0530

    Login Page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import {
     Card,
@@ -16,61 +16,112 @@ import {
     TabsList,
     TabsTrigger,
 } from "@/components/ui/tabs"
+import { Eye, EyeClosed, EyeOff } from 'lucide-react'
 
 const Login = () => {
+    const [password, setPassword] = useState(false);
+    const [input, setInput] = useState({
+        name: "",
+        email: '',
+        password: ''
+    });
+
+    const handleInputChange = (e) => {
+        setInput({ ...input, [e.target.name]: e.target.value })
+    };
+
+    const checkPassword = () => {
+        if (!password) {
+            setPassword(true);
+        } else {
+            setPassword(false);
+        }
+    }
     return (
-        <Tabs defaultValue="account" className="w-[400px]">
-            <TabsList className="grid w-full grid-cols-2">
-                <TabsTrigger value="account">Account</TabsTrigger>
-                <TabsTrigger value="password">Password</TabsTrigger>
-            </TabsList>
-            <TabsContent value="account">
-                <Card>
-                    <CardHeader>
-                        <CardTitle>Account</CardTitle>
-                        <CardDescription>
-                            Make changes to your account here. Click save when you're done.
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent className="space-y-2">
-                        <div className="space-y-1">
-                            <Label htmlFor="name">Name</Label>
-                            <Input id="name" defaultValue="Pedro Duarte" />
-                        </div>
-                        <div className="space-y-1">
-                            <Label htmlFor="username">Username</Label>
-                            <Input id="username" defaultValue="@peduarte" />
-                        </div>
-                    </CardContent>
-                    <CardFooter>
-                        <Button>Save changes</Button>
-                    </CardFooter>
-                </Card>
-            </TabsContent>
-            <TabsContent value="password">
-                <Card>
-                    <CardHeader>
-                        <CardTitle>Password</CardTitle>
-                        <CardDescription>
-                            Change your password here. After saving, you'll be logged out.
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent className="space-y-2">
-                        <div className="space-y-1">
-                            <Label htmlFor="current">Current password</Label>
-                            <Input id="current" type="password" />
-                        </div>
-                        <div className="space-y-1">
-                            <Label htmlFor="new">New password</Label>
-                            <Input id="new" type="password" />
-                        </div>
-                    </CardContent>
-                    <CardFooter>
-                        <Button>Save password</Button>
-                    </CardFooter>
-                </Card>
-            </TabsContent>
-        </Tabs>
+        <div className='max-w-6xl mx-auto h-[100%]'>
+            <div className='flex justify-center items-center '>
+                <Tabs defaultValue="login" className="w-[400px]">
+                    <TabsList className="grid w-full grid-cols-2">
+                        <TabsTrigger value="signup">Signup</TabsTrigger>
+                        <TabsTrigger value="login">Login</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="signup">
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>SignUp</CardTitle>
+                                <CardDescription>
+                                    Create a new account and click signup when you're done.
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent className="space-y-2">
+                                <div className="space-y-1">
+                                    <Label htmlFor="name">Name</Label>
+                                    <Input id="name"
+                                        name="name"
+                                        type="text"
+                                        value={input.name}
+                                        onChange={handleInputChange} />
+                                </div>
+                                <div className="space-y-1">
+                                    <Label htmlFor="email">Email</Label>
+                                    <Input id="email"
+                                        name="email"
+                                        type="email"
+                                        value={input.email}
+                                        onChange={handleInputChange} />
+                                </div>
+                                <div className="space-y-1">
+                                    <Label htmlFor="password">Password</Label>
+                                    <div className='flex items-center gap-2 '>
+                                        <Input id="password"
+                                            type={password ? "text" : "password"}
+                                            name="password"
+                                            value={input.password}  
+                                            onChange={handleInputChange} />
+                                        {
+                                            password ? <Eye
+                                                onClick={checkPassword}
+                                                className='cursor-pointer' />
+                                                : <EyeOff
+                                                    onClick={checkPassword}
+                                                    className='cursor-pointer' />
+                                        }
+                                    </div>
+                                </div>
+
+                            </CardContent>
+                            <CardFooter>
+                                <Button>Signup</Button>
+                            </CardFooter>
+                        </Card>
+                    </TabsContent>
+                    <TabsContent value="login">
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>Login</CardTitle>
+                                <CardDescription>
+                                    Login your password here , After signup , you'll be logged in.
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent className="space-y-2">
+                                <div className="space-y-1">
+                                    <Label htmlFor="current">Current password</Label>
+                                    <Input id="current" type="password" />
+                                </div>
+                                <div className="space-y-1">
+                                    <Label htmlFor="new">New password</Label>
+                                    <Input id="new" type="password" />
+                                </div>
+                            </CardContent>
+                            <CardFooter>
+                                <Button>Login</Button>
+                            </CardFooter>
+                        </Card>
+                    </TabsContent>
+                </Tabs>
+            </div>
+
+        </div>
     )
 }
 
