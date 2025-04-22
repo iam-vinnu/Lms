e535b5a9d5dde25aab837f25d54d54aaaa5634d6
iam-vinnu <binaybehera101@gmail.com>
2025-04-22 10:50:32 +0530

    added button

---
+++ client/src/components/PurchasedButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from './ui/button'
+
+const PurchasedButton = () => {
+  return (
+        <Button className="w-full">Purchase Course</Button>
+  )
+}
+
+export default PurchasedButton
--- client/src/pages/student/Course.jsx
+++ client/src/pages/student/Course.jsx
@@ -2,9 +2,11 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Badge } from '@/components/ui/badge'
 import { Card, CardContent } from '@/components/ui/card'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Course({course}) {
   return (
+    <Link to={`course-details/${course._id}`}>
     <Card className="overflow-hidden rounded-lg dark:bg-gray-800 bg-white hover:shadow-2xl transform hover:scale-105 transition-all duration-300">
       <div className='relative' >
         <img src={course?.courseThumbnail} alt="course" className='rounded-t-lg w-full h-36 object-cover' />
@@ -32,6 +34,7 @@ function Course({course}) {
         </div>
       </CardContent>
     </Card>
+    </Link>
   )
 }
 
